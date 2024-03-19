f41ac06a3749d75e486ae0d989c6d6c0e0254386
Dylan <dderose@odic.com>
2024-03-19 11:11:48 -0400

    i2c prototyping

---
+++ application/solution/i2c_hal_freertos.h
@@ -0,0 +1,30 @@
+#ifndef I2C_HAL_FREERTOS_H__
+#define I2C_HAL_FREERTOS_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define I2C_SYSTEM_INSTANCE_ID 0
+#define I2C_SENSOR_INSTANCE_ID 1
+
+typedef enum {
+    i2c_interface_system,
+    i2c_interface_sensors,
+    i2c_num_interfaces
+} i2c_interface_selection_t;
+
+typedef enum {
+    i2c_speed_100k,
+    i2c_speed_400k,
+    i2c_speed_number_of_selections
+} i2c_speed_selection_t;
+
+#define I2C_INSTANCE_ID 1
+#ifdef __cplusplus
+}
+#endif
+#endif // I2C_HAL_FREERTOS_H__
--- application/src/tsys01.c
+++ application/src/tsys01.c
@@ -2,7 +2,7 @@
 #include <stdbool.h>
 #include <math.h>
 #include <string.h>
-#include "tsys03.h"
+#include "tsys01.h"
 #include "i2c_hal.h"
 #include "delay_hal.h"
 #include "app_scheduler.h"
@@ -17,69 +17,69 @@ typedef struct {
     uint8_t regSelection;
     uint8_t readLength;
     uint8_t readData[READ_BUFFER_SIZE];
-} tsys03_init_op_read_buffer_t;
+} tsys01_init_op_read_buffer_t;
 
 typedef enum {
-    tsys03_init_read_op,
-    tsys03_init_write_op,
-    tsys03_reset_op,
-    tsys03_read_serial_op,
-    tsys03_start_conversion_op,
-    tsys03_read_adc_op
-} tsys03_internal_op_type_t;
+    tsys01_init_read_op,
+    tsys01_init_write_op,
+    tsys01_reset_op,
+    tsys01_read_serial_op,
+    tsys01_start_conversion_op,
+    tsys01_read_adc_op
+} tsys01_internal_op_type_t;
 
 static bool sensorIsBusy = false;
-static tsys03_internal_op_type_t current_op_type = tsys03_init_read_op;
+static tsys01_internal_op_type_t current_op_type = tsys01_init_read_op;
 
-static tsys03_opDoneCallback_t savedUserCallback = NULL;
+static tsys01_opDoneCallback_t savedUserCallback = NULL;
 
 static uint8_t writeBuffer = 0;
-static tsys03_init_op_read_buffer_t readBuffer = {0};
+static tsys01_init_op_read_buffer_t readBuffer = {0};
 static i2c_status_t initOpOutcome = i2c_success;
 static bool conversionStarted = false;
 
-static tsys03_errors_t tsys03_readRegister(uint8_t reg, tsys03_internal_op_type_t ts_operation, tsys03_opDoneCallback_t cb);
+static tsys01_errors_t tsys01_readRegister(uint8_t reg, tsys01_internal_op_type_t ts_operation, tsys01_opDoneCallback_t cb);
 
 
-void tsys03i2cDoneCallback(i2c_status_t outcome, uint8_t data_len, uint8_t * p_data_read)
+void tsys01i2cDoneCallback(i2c_status_t outcome, uint8_t data_len, uint8_t * p_data_read)
 {
-    tsys03_callback_data_t userData;
+    tsys01_callback_data_t userData;
     sensorIsBusy = false;
     
     switch (current_op_type) {
         default:
             // Never reached
             break;
-        case tsys03_init_read_op:
+        case tsys01_init_read_op:
             initOpOutcome = outcome;
             // Data was written directly to buffer, just record number of bytes actually transferred
             readBuffer.readLength = data_len;
             break;
-        case tsys03_init_write_op:
+        case tsys01_init_write_op:
             initOpOutcome = outcome;
             break;
-        case tsys03_reset_op:
+        case tsys01_reset_op:
             initOpOutcome = outcome;
             break;
-        case tsys03_start_conversion_op:
+        case tsys01_start_conversion_op:
             initOpOutcome = outcome;
             conversionStarted = true;
-            //savedUserCallback(tsys03_success, &userData);
+            //savedUserCallback(tsys01_success, &userData);
             break;
-        case tsys03_read_adc_op:
+        case tsys01_read_adc_op:
         {
             if (savedUserCallback != NULL) {
-                userData.data_type = tsys03_read_adc;
+                userData.data_type = tsys01_read_adc;
                 if (outcome != i2c_success) {
-                    savedUserCallback(tsys03_i2c_error, &userData);
+                    savedUserCallback(tsys01_i2c_error, &userData);
                 } else if (data_len != 3) {
-                    savedUserCallback(tsys03_i2c_error, &userData);
+                    savedUserCallback(tsys01_i2c_error, &userData);
                 } else {
                     uint16_t adcRaw = ((uint16_t) readBuffer.readData[0] << 8) + ((uint16_t) readBuffer.readData[1]);
                     double temperature = -40 + adcRaw  * 165 / 65535.0;
                     userData.data.temperature = temperature;
                     userData.data.crc = readBuffer.readData[2];
-                    savedUserCallback(tsys03_success, &userData);
+                    savedUserCallback(tsys01_success, &userData);
                 }
             }
             break;
@@ -87,12 +87,12 @@ void tsys03i2cDoneCallback(i2c_status_t outcome, uint8_t data_len, uint8_t * p_d
     }
 }
 
-static tsys03_errors_t tsys03_writeRegister(uint8_t reg, 
-                                                   tsys03_internal_op_type_t ts_operation, 
-                                                   tsys03_opDoneCallback_t cb)
+static tsys01_errors_t tsys01_writeRegister(uint8_t reg, 
+                                                   tsys01_internal_op_type_t ts_operation, 
+                                                   tsys01_opDoneCallback_t cb)
 {
     if (sensorIsBusy) {
-        return tsys03_busy;
+        return tsys01_busy;
     }
     
     sensorIsBusy = true;
@@ -102,8 +102,8 @@ static tsys03_errors_t tsys03_writeRegister(uint8_t reg,
     i2c_write_transaction_t i2c_trans_info;
     i2c_trans_info.p_write_data = &writeBuffer;
     i2c_trans_info.write_len = 1;
-    i2c_trans_info.addr = TSYS03_ADDR;
-    i2c_trans_info.cb = tsys03i2cDoneCallback;
+    i2c_trans_info.addr = tsys01_ADDR;
+    i2c_trans_info.cb = tsys01i2cDoneCallback;
     i2c_trans_info.stop = true; //single write then stop
 
     current_op_type = ts_operation;
@@ -113,18 +113,18 @@ static tsys03_errors_t tsys03_writeRegister(uint8_t reg,
     if (i2c_trans_status != i2c_success) {
         sensorIsBusy = false;
         if (i2c_trans_status == i2c_busy) {
-            return tsys03_busy;
+            return tsys01_busy;
         }
-        return tsys03_i2c_error;
+        return tsys01_i2c_error;
     }
     
-    return tsys03_success;
+    return tsys01_success;
 }
 
-static tsys03_errors_t tsys03_readRegister(uint8_t reg, tsys03_internal_op_type_t ts_operation, tsys03_opDoneCallback_t cb)
+static tsys01_errors_t tsys01_readRegister(uint8_t reg, tsys01_internal_op_type_t ts_operation, tsys01_opDoneCallback_t cb)
 {
     if (sensorIsBusy) {
-        return tsys03_busy;
+        return tsys01_busy;
     }
     
     sensorIsBusy = true;
@@ -134,18 +134,18 @@ static tsys03_errors_t tsys03_readRegister(uint8_t reg, tsys03_internal_op_type_
 
     //setup I2C read
     readBuffer.regSelection = reg;
-    i2c_trans_info.addr = TSYS03_ADDR;
+    i2c_trans_info.addr = tsys01_ADDR;
     i2c_trans_info.p_register_selection = &readBuffer.regSelection;
     i2c_trans_info.register_len = 1;
     i2c_trans_info.p_read_data = &readBuffer.readData[0];
-    if (ts_operation == tsys03_read_adc_op)
+    if (ts_operation == tsys01_read_adc_op)
     {
         i2c_trans_info.read_len = 3;
-    } else if (ts_operation == tsys03_read_serial_op) {
+    } else if (ts_operation == tsys01_read_serial_op) {
         i2c_trans_info.read_len = 4;
     } 
     //i2c_trans_info.read_len = 3;
-    i2c_trans_info.cb = tsys03i2cDoneCallback;
+    i2c_trans_info.cb = tsys01i2cDoneCallback;
 
     current_op_type = ts_operation;
     savedUserCallback = cb;
@@ -154,20 +154,20 @@ static tsys03_errors_t tsys03_readRegister(uint8_t reg, tsys03_internal_op_type_
     if (i2c_trans_status != i2c_success) {
         sensorIsBusy = false;
         if (i2c_trans_status == i2c_busy) {
-            return tsys03_busy;
+            return tsys01_busy;
         }
-        return tsys03_i2c_error;
+        return tsys01_i2c_error;
     }
   
-    return tsys03_success;
+    return tsys01_success;
 }
 
-tsys03_errors_t tsys03_startConversion(tsys03_opDoneCallback_t cb)
+tsys01_errors_t tsys01_startConversion(tsys01_opDoneCallback_t cb)
 {
-    return tsys03_writeRegister(TSYS03_START_CONVERSION_COMMAND, tsys03_start_conversion_op, cb);
+    return tsys01_writeRegister(tsys01_START_CONVERSION_COMMAND, tsys01_start_conversion_op, cb);
 }
 
-tsys03_errors_t tsys03_getTemp(tsys03_opDoneCallback_t cb)
+tsys01_errors_t tsys01_getTemp(tsys01_opDoneCallback_t cb)
 {
-    return tsys03_readRegister(TSYS03_READ_ADC_COMMAND, tsys03_read_adc_op, cb);
+    return tsys01_readRegister(tsys01_READ_ADC_COMMAND, tsys01_read_adc_op, cb);
 }
