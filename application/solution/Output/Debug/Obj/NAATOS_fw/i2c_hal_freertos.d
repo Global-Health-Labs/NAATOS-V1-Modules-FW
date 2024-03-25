bdb507f0dcba453da239f2509863273759c199c1
Liam Sullivan <liam@odic.com>
2024-03-25 17:06:27 -0400

    included i2c classes inside of build

---
+++ application/solution/Output/Debug/Obj/NAATOS_fw/i2c_hal_freertos.d
@@ -0,0 +1,64 @@
+Output/Debug/Obj/NAATOS_fw/i2c_hal_freertos.o: \
+ D\:\NAATOS\Source\ Code\GHL-NAATOS\application\src\i2c_hal_freertos.c \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stdint.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stdbool.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stddef.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/__crossworks.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/string.h \
+ D\:\NAATOS\Source\ Code\GHL-NAATOS\application\src\i2c_hal_freertos.h \
+ ../sdk/components/libraries/util/sdk_errors.h \
+ ../sdk/components/drivers_nrf/nrf_soc_nosd/nrf_error.h \
+ ../sdk/integration/nrfx/legacy/nrf_drv_twi.h ../sdk/modules/nrfx/nrfx.h \
+ ../sdk/integration/nrfx/nrfx_config.h ../src/config/sdk_config.h \
+ ../sdk/modules/nrfx/drivers/nrfx_common.h ../sdk/modules/nrfx/mdk/nrf.h \
+ ../sdk/modules/nrfx/mdk/nrf52840.h \
+ ../sdk/components/toolchain/cmsis/include/core_cm4.h \
+ ../sdk/components/toolchain/cmsis/include/cmsis_version.h \
+ ../sdk/components/toolchain/cmsis/include/cmsis_compiler.h \
+ ../sdk/components/toolchain/cmsis/include/cmsis_gcc.h \
+ ../sdk/components/toolchain/cmsis/include/mpu_armv7.h \
+ ../sdk/modules/nrfx/mdk/system_nrf52840.h \
+ ../sdk/modules/nrfx/mdk/nrf52840_bitfields.h \
+ ../sdk/modules/nrfx/mdk/nrf51_to_nrf52840.h \
+ ../sdk/modules/nrfx/mdk/nrf52_to_nrf52840.h \
+ ../sdk/modules/nrfx/mdk/compiler_abstraction.h \
+ ../sdk/modules/nrfx/mdk/nrf_peripherals.h \
+ ../sdk/modules/nrfx/mdk/nrf52840_peripherals.h \
+ ../sdk/integration/nrfx/nrfx_glue.h \
+ ../sdk/integration/nrfx/legacy/apply_old_config.h \
+ ../sdk/modules/nrfx/soc/nrfx_irqs.h \
+ ../sdk/modules/nrfx/soc/nrfx_irqs_nrf52840.h \
+ ../sdk/components/libraries/util/nrf_assert.h \
+ ../sdk/components/libraries/util/app_util.h \
+ ../sdk/modules/nrfx/mdk/compiler_abstraction.h \
+ ../sdk/components/libraries/util/nordic_common.h \
+ ../sdk/components/libraries/util/nordic_common.h \
+ ../sdk/components/libraries/util/app_util_platform.h \
+ ../sdk/components/libraries/util/nrf_assert.h \
+ ../sdk/components/libraries/util/app_error.h \
+ C\:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.42a/include/stdio.h \
+ ../sdk/components/libraries/util/sdk_errors.h \
+ ../sdk/components/libraries/util/app_error_weak.h \
+ ../sdk/modules/nrfx/soc/nrfx_coredep.h \
+ ../sdk/modules/nrfx/soc/nrfx_atomic.h \
+ ../sdk/components/libraries/util/sdk_resources.h \
+ ../sdk/modules/nrfx/drivers/nrfx_errors.h \
+ ../sdk/modules/nrfx/drivers/include/nrfx_twim.h \
+ ../sdk/modules/nrfx/drivers/include/nrfx_twi_twim.h \
+ ../sdk/modules/nrfx/hal/nrf_twim.h \
+ ../sdk/modules/nrfx/drivers/include/nrfx_twi.h \
+ ../sdk/modules/nrfx/hal/nrf_twi.h ../sdk/modules/nrfx/hal/nrf_gpio.h \
+ ../sdk/external/freertos/source/include/FreeRTOS.h \
+ ../sdk/external/freertos/config/FreeRTOSConfig.h \
+ ../sdk/external/freertos/source/include/projdefs.h \
+ ../sdk/external/freertos/source/include/portable.h \
+ ../sdk/external/freertos/source/include/deprecated_definitions.h \
+ ../sdk/external/freertos/portable/GCC/nrf52/portmacro.h \
+ ../sdk/external/freertos/portable/CMSIS/nrf52/portmacro_cmsis.h \
+ ../sdk/external/freertos/source/include/mpu_wrappers.h \
+ ../sdk/external/freertos/source/include/task.h \
+ ../sdk/external/freertos/source/include/list.h \
+ ../sdk/external/freertos/source/include/timers.h \
+ ../sdk/external/freertos/source/include/task.h \
+ ../sdk/external/freertos/source/include/semphr.h \
+ ../sdk/external/freertos/source/include/queue.h
