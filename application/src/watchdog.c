f44990f3b01e3b5260ffff4bdc6dfc8d7df962ea
Rob <rob@odic.com>
2024-05-21 07:57:42 -0400

    adding in heater and battery cases  to watchdog

--- application/src/watchdog.c
+++ application/src/watchdog.c
@@ -13,10 +13,9 @@ void wdt_event_handler(void) {
 void watchdog_init(void) {
   uint32_t err_code = NRF_SUCCESS;
 
-  // Configure the WDT with a timeout of 5000 ms
   nrf_drv_wdt_config_t config = NRF_DRV_WDT_DEAFULT_CONFIG;
   config.behaviour = NRF_WDT_BEHAVIOUR_RUN_SLEEP_HALT; // Ensure this behaviour is supported
-  config.reload_value = 10000; // 10 seconds
+  config.reload_value = 12000; // 12 seconds
   err_code = nrf_drv_wdt_init(&config, wdt_event_handler);
   APP_ERROR_CHECK(err_code);
 
@@ -27,6 +26,7 @@ void watchdog_init(void) {
   nrf_drv_wdt_enable();
 }
 
+#define MAX_WDT_TASK_TIMEOUT_S 10 // SECONDS
 
 void wdtFeedTask(void * pvParameters) {
 /* We can handle this a few ways. We can just kick the dog in a task on an interval. That would support general system lockup 
@@ -36,31 +36,55 @@ void wdtFeedTask(void * pvParameters) {
  *
  *
  **/
-  (void) pvParameters;
+(void) pvParameters;
+watchdog_time_update_t recv_req;
 
- watchdog_time_update_t recv_req;
+bool heaterValid = false;
+int heaterTicks = 0;
 
-  while (true) {
+bool batteryValid = false;
+int batteryTicks = 0;
+
+while (true) {
 
     //if msg in queue
     // Set watchdog time and or set valid flag
     if( xQueueReceive( watchdog_rxTimesQueue,
                          &recv_req,
-                         ( TickType_t ) 0 ) == pdPASS )
-      {
+                         ( TickType_t ) 0 ) == pdPASS ) {
          /* *pxRxedPointer now points to xMessage. */
          // reset specific task counter to 0
          // if set to invalid then we dont have to compare times
-      }
 
-    // Consider all valid tasks for timeout. This means if their  tick has been incremented past the max  value we dont kick the dog
-    // If task not valid skip and dont consider it for WDT 
+         switch(recv_req.taskName) {
+         case BATTERY:
+          batteryValid = recv_req.valid;
+          batteryTicks = 0;
+         break;
 
-    // if no tasks are valid just kick the dog
-    // if one of the tasks is missed then dont allow the dog to be kicked
+         case HEATER:
+          heaterValid = recv_req.valid;
+          heaterTicks = 0;
+         break;
 
-    // Feed the watchdog
-    nrf_drv_wdt_channel_feed(m_channel_id);
+         default:
+         break;
+
+         }
+    }
+
+    if(heaterValid){
+      heaterTicks++;
+    }
+
+    if(batteryValid){
+      batteryTicks++;
+    }
+
+
+    if(heaterTicks < MAX_WDT_TASK_TIMEOUT_S && batteryTicks < MAX_WDT_TASK_TIMEOUT_S) {
+      nrf_drv_wdt_channel_feed(m_channel_id);
+    }
 
     // Delay for a period shorter than the watchdog timeout
     vTaskDelay(1000); 
