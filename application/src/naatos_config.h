1de4f0129f63bbf1289e967b9900aea1afa19604
Rob <rob@odic.com>
2024-06-10 10:10:35 -0400

    V1.0.0

--- application/src/naatos_config.h
+++ application/src/naatos_config.h
@@ -5,7 +5,7 @@
 #include "FreeRTOS.h"
 #include  "task.h"
 
-#define NAATOS_FW_VERSON  "v0.1.0_c"
+#define NAATOS_FW_VERSON  "V1.0.0"
 
 #define pdTICKS_TO_MS( xTimeInTicks )    ( ( TickType_t ) ( ( ( uint64_t ) ( xTimeInTicks ) * ( uint64_t ) 1000U ) / ( uint64_t ) configTICK_RATE_HZ ) )
 
