dd73591f13a70d79828ab9253b81ec2aa7f26372
Liam Sullivan <liam@odic.com>
2024-11-14 10:04:44 -0500

    Put application space to 0x0000

--- application/src/led.c
+++ application/src/led.c
@@ -84,6 +84,7 @@ void handleLedState(LEDRxQueueMsg_t ledMsg) {
       set_led1_red_slow_blink();
       set_led2_red_slow_blink();
     } else if (ledFlags.testAbort) {
+      turn_off_led2_blue_animation();
       set_led1_red_fast_blink();
       set_led2_red_fast_blink();
     } else if (ledFlags.testComplete) {
@@ -134,6 +135,9 @@ void handleLedState(LEDRxQueueMsg_t ledMsg) {
 void set_led1_green_solid(void) {
 #if ENABLE_LEDS
   led_driver_disable_channel(LED1, led1_current_color, NULL);
+  led_driver_disable_channel(LED2, red, NULL);
+  led_driver_disable_channel(LED2, green, NULL);
+  led_driver_disable_channel(LED2, blue, NULL);
   led_driver_enable_channel(LED1, green, NULL);
   led_driver_set_channel_animation_solid(LED1, green, true, NULL);
   led1_current_color = green;
@@ -143,6 +147,9 @@ void set_led1_green_solid(void) {
 void set_led1_green_breathe(void) {
 #if ENABLE_LEDS
   led_driver_disable_channel(LED1, led1_current_color, NULL);
+  led_driver_disable_channel(LED2, red, NULL);
+  led_driver_disable_channel(LED2, green, NULL);
+  led_driver_disable_channel(LED2, blue, NULL);
   led_driver_enable_channel(LED1, green, NULL);
   led_driver_set_channel_animation_breathing(LED1, green, true, NULL);
   led1_current_color = green;
@@ -152,6 +159,9 @@ void set_led1_green_breathe(void) {
 void set_led1_blue_slow_blink(void) {
 #if ENABLE_LEDS
   led_driver_disable_channel(LED1, led1_current_color, NULL);
+  led_driver_disable_channel(LED2, red, NULL);
+  led_driver_disable_channel(LED2, green, NULL);
+  led_driver_disable_channel(LED2, blue, NULL);
   led_driver_enable_channel(LED1, blue, NULL);
   led_driver_set_channel_animation_flashing(LED1, blue, true, NULL);
   led1_current_color = blue;
@@ -161,6 +171,9 @@ void set_led1_blue_slow_blink(void) {
 void set_led1_red_slow_blink(void) {
 #if ENABLE_LEDS
   led_driver_disable_channel(LED1, led1_current_color, NULL);
+  led_driver_disable_channel(LED2, red, NULL);
+  led_driver_disable_channel(LED2, green, NULL);
+  led_driver_disable_channel(LED2, blue, NULL);
   led_driver_enable_channel(LED1, red, NULL);
   led_driver_set_channel_animation_flashing(LED1, red, true, NULL);
   led1_current_color = red;
@@ -170,6 +183,9 @@ void set_led1_red_slow_blink(void) {
 void set_led1_red_fast_blink(void) {
 #if ENABLE_LEDS
   led_driver_disable_channel(LED1, led1_current_color, NULL);
+  led_driver_disable_channel(LED2, red, NULL);
+  led_driver_disable_channel(LED2, green, NULL);
+  led_driver_disable_channel(LED2, blue, NULL);
   led_driver_enable_channel(LED1, red, NULL);
   led_driver_set_channel_animation_flashing(LED1, red, true, NULL);
   led1_current_color = red;
@@ -197,6 +213,9 @@ void set_led1_red_solid(void) {
 void set_led2_red_slow_blink(void) {
 #if ENABLE_LEDS
   led_driver_disable_channel(LED2, led1_current_color, NULL);
+  led_driver_disable_channel(LED2, red, NULL);
+  led_driver_disable_channel(LED2, green, NULL);
+  led_driver_disable_channel(LED2, blue, NULL);
   led_driver_enable_channel(LED2, red, NULL);
   led_driver_set_channel_animation_flashing(LED2, red, true, NULL);
   led2_current_color = red;
@@ -206,6 +225,9 @@ void set_led2_red_slow_blink(void) {
 void set_led2_red_fast_blink(void) {
 #if ENABLE_LEDS
   led_driver_disable_channel(LED2, led2_current_color, NULL);
+  led_driver_disable_channel(LED2, red, NULL);
+  led_driver_disable_channel(LED2, green, NULL);
+  led_driver_disable_channel(LED2, blue, NULL);
   led_driver_enable_channel(LED2, red, NULL);
   led_driver_set_channel_animation_flashing(LED2, red, true, NULL);
   led2_current_color = red;
@@ -262,6 +284,9 @@ void set_led2_red_solid(void) {
 void set_led1_blue_breathe(void) {
 #if ENABLE_LEDS
   led_driver_disable_channel(LED1, led1_current_color, NULL);
+  led_driver_disable_channel(LED2, red, NULL);
+  led_driver_disable_channel(LED2, green, NULL);
+  led_driver_disable_channel(LED2, blue, NULL);
   led_driver_enable_channel(LED1, blue, NULL);
   led_driver_set_channel_animation_breathing(LED1, blue, true, NULL);
   led1_current_color = blue;
@@ -283,6 +308,9 @@ void set_led2_blue_breathe(void) {
 void turn_off_led2_blue_animation(void) {
 #if ENABLE_LEDS
   led_driver_disable_channel(LED2, led2_current_color, NULL);
+  led_driver_disable_channel(LED2, red, NULL);
+  led_driver_disable_channel(LED2, green, NULL);
+  led_driver_disable_channel(LED2, blue, NULL);
   led_driver_disable_animation(LED2, led2_current_color, NULL);
 #endif
 }
