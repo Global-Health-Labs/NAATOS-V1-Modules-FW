58c68c46c5a643c433379ab3a8a9158574d88d01
Liam Sullivan <liam@odic.com>
2024-12-06 11:01:41 -0500

    Version bump V2.6b

--- application/src/naatos_config.h
+++ application/src/naatos_config.h
@@ -5,7 +5,7 @@
 #include "task.h"
 #include <stdint.h>
 
-#define VERSION "2.5b"
+#define VERSION "2.6b"
 
 /*Define this when building sample prep only otherwise comment out*/
 #define SAMPLE_PREP_BOARD
