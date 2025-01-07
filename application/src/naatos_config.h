ee3b681cecf0dc1a5da5a4825c9d0d06ae147236
Liam Sullivan <liam@odic.com>
2025-01-07 14:36:49 -0500

    version wasnt updated. Most recent Sample Prep code without PM code. Going on Rev C

--- application/src/naatos_config.h
+++ application/src/naatos_config.h
@@ -5,7 +5,7 @@
 #include "task.h"
 #include <stdint.h>
 
-#define VERSION "2.7b"
+#define VERSION "2.8b"
 
 /*Define this when building sample prep only otherwise comment out*/
 #define SAMPLE_PREP_BOARD
