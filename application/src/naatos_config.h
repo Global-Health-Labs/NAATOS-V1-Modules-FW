4aad307395cff8fa58ac9665af354155f124771f
Simon Ghionea <simon.ghionea@ghlabs.org>
2025-01-31 11:47:12 -0800

    bump ghl version number

--- application/src/naatos_config.h
+++ application/src/naatos_config.h
@@ -5,7 +5,7 @@
 #include "task.h"
 #include <stdint.h>
 
-#define VERSION "3.0"
+#define VERSION "3.0bghl"
 
 /*Define this when building sample prep only otherwise comment out*/
 #define SAMPLE_PREP_BOARD
